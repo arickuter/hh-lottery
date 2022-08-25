7fb64a67b7b3989750578ea67fce01f2765e4d91
Aric Kuter <arickuter99@gmail.com>
2022-08-25 10:56:39 +0200

    deploy complete

--- deploy/01_deploy_lottery.ts
+++ deploy/01_deploy_lottery.ts
@@ -5,6 +5,10 @@ import {
   developmentChains,
   VERIFICATION_BLOCK_CONFIRMATIONS,
 } from "../helper_hardhat_config";
+import { ethers } from "hardhat";
+import verify from "../utils/verify";
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
 
 const deployLottery: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
@@ -13,18 +17,50 @@ const deployLottery: DeployFunction = async function (
   const { getNamedAccounts, deployments, network } = hre;
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-  const chainId: number = network.config.chainId!;
   const waitBlockConfirmations = developmentChains.includes(network.name)
     ? 1
     : VERIFICATION_BLOCK_CONFIRMATIONS;
+  let vrfCoordinatorV2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const txRes = await vrfCoordinatorV2Mock.createSubscription();
+    const txRec = await txRes.wait(1);
+    subscriptionId = txRec.events[0].args.subId;
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[network.name]["vrfCoordinatorV2"];
+    subscriptionId = networkConfig[network.name]["subscriptionId"];
+  }
+  /* lottery args */
+  const entranceFee = ethers.utils.parseEther(
+    networkConfig[network.name]["entranceFee"]!
+  );
+  const keyHash = networkConfig[network.name]["keyHash"];
+  const callbackGasLimit = networkConfig[network.name]["callbackGasLimit"];
+  const interval = networkConfig[network.name]["interval"];
+
+  const args = [
+    entranceFee,
+    vrfCoordinatorV2Address,
+    keyHash,
+    subscriptionId,
+    callbackGasLimit,
+    interval,
+  ];
 
   log("----------------------------------------------------");
   log("Deploying Lottery and waiting for confirmations...");
   const lottery = await deploy("Lottery", {
     from: deployer,
-    args: [],
+    args: args,
     log: true,
-    // we need to wait if on a live network so we can verify properly
     waitConfirmations: waitBlockConfirmations,
   });
   log(`Lottery deployed at ${lottery.address}`);
@@ -32,6 +68,7 @@ const deployLottery: DeployFunction = async function (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
   ) {
+    await verify(lottery.address, args);
   }
 };
 export default deployLottery;
--- helper_hardhat_config.ts
+++ helper_hardhat_config.ts
@@ -1,9 +1,9 @@
 export interface networkConfigItem {
   name?: string;
   subscriptionId?: string;
-  gasLane?: string;
-  keepersUpdateInterval?: string;
-  raffleEntranceFee?: string;
+  keyHash?: string;
+  interval?: string;
+  entranceFee?: string;
   callbackGasLimit?: string;
   vrfCoordinatorV2?: string;
 }
@@ -13,25 +13,33 @@ export interface networkConfigInfo {
 }
 
 export const networkConfig: networkConfigInfo = {
+  hardhat: {
+    subscriptionId: "588",
+    keyHash:
+      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+    interval: "30",
+    entranceFee: "100000000000000000", // 0.1 ETH
+    callbackGasLimit: "500000", // 500,000 gas
+  },
   localhost: {
     subscriptionId: "588",
-    gasLane:
+    keyHash:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-    keepersUpdateInterval: "30",
-    raffleEntranceFee: "100000000000000000", // 0.1 ETH
+    interval: "30",
+    entranceFee: "100000000000000000", // 0.1 ETH
     callbackGasLimit: "500000", // 500,000 gas
   },
   rinkeby: {
     subscriptionId: "588",
-    gasLane:
+    keyHash:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-    keepersUpdateInterval: "30",
-    raffleEntranceFee: "100000000000000000", // 0.1 ETH
+    interval: "30",
+    entranceFee: "100000000000000000", // 0.1 ETH
     callbackGasLimit: "500000", // 500,000 gas
     vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
   },
   mainnet: {
-    keepersUpdateInterval: "30",
+    interval: "30",
   },
 };
 export const VERIFICATION_BLOCK_CONFIRMATIONS = 5;
